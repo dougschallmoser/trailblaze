0162d467df27bd30786924345418d96d84713bc3
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-07 10:53:48 -0800

    add auth controller to handle logging in

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+
   def create
     user = User.new(user_params)
     if user.save
