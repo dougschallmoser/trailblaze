1171b8c7e0c74cb33010578748e633f12c3112c3
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-07 13:29:40 -0800

    add navbar component with imports of bootstrap components

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <Navbar collapseOnSelect expand="md" fixed="top">
+      <Navbar.Brand><Link to="/">Trailblaze</Link></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+          <Nav.Link as={Link} to="/login">Login</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
