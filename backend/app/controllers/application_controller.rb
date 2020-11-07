6717bd58ce8e96fe617c29698438c9c063f1710a
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-07 10:01:29 -0800

    add methods for encoding and decoding JWT tokens

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,37 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
+
+  def encode_token(payload)
+    JWT.encode(payload, 'trailblaze_app')
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decode_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'trailblaze_app', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def current_user
+    if decode_token 
+      user_id = decode_token[0]['user_id']
+      user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized 
+    render json: { message: 'Log in to continue' }, status: :unauthorized unless logged_in?
+  end
 end
