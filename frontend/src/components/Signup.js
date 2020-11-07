0e8b19bec07fa779410de290dc3f59c37d462203
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-07 14:55:11 -0800

    add form to signup component

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,8 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 
 const Signup = () => {
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+    bio: '',
+    avatar: ''
+  })
+
+  const handleChange = (event) => {
+    setUser(prevUser => {
+      return {
+        ...prevUser, [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('here')
+
+  }
+
   return (
-    <div className="container">Signup</div>
+    <div className="container signup">
+      <h2>Signup Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Username:</label>
+          <input 
+            className="form-control"
+            type="text"
+            name='username'
+            value={user.username}
+            onChange={handleChange}
+          /><br/>
+        </div>
+
+        <div className="form-group">
+          <label>Password:</label>
+          <input 
+            className="form-control"
+            type="password"
+            name='password'
+            value={user.password}
+            onChange={handleChange}
+          /><br/>
+        </div>
+
+        <div className="form-group">
+          <label>Bio:</label>
+          <textarea 
+            className="form-control"
+            name='bio'
+            value={user.bio}
+            onChange={handleChange}
+          /><br/>
+        </div>
+        
+        <div className="form-group">
+          <label>Avatar:</label>
+          <input 
+            className="form-control"
+            type="text"
+            name='avatar'
+            value={user.avatar}
+            onChange={handleChange}
+          /><br/>
+        </div>
+
+        <input type="submit" className="btn btn-primary"></input>
+      </form>
+    </div>
   )
 }
 
