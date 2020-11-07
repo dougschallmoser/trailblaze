d787e108a2873b74b3690634759af34994633af5
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-07 10:01:06 -0800

    add to create method, render json if statement logic

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
 
   def create
     user = User.new(user_params)
     if user.save
-      render json: user, status: 200
+      token = encode_token(user_id: user.id)
+      render json: { user: UserSerializer.new(user), jwt: token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
