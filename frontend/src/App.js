b70b85266d952f5e4a97163faf22508f4e71338d
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-07 13:28:40 -0800

    add routes, import bootstrap css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import NavBar from './components/NavBar';
 import SearchContainer from './containers/SearchContainer';
