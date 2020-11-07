ac3099eebe4f1bd6a1c8aaf20cc3930f8422aecb
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-07 11:32:56 -0800

    implement render method with provider and store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import reducer from './reducers';
 import App from './App';
 
+let store = createStore(reducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <App />, document.querySelector('#main')
-)
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>, 
+  document.querySelector('#main')
+);
