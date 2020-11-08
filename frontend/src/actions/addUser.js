0eeeff57f5ea20e3ca6cea66f1f99da246e22430
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-08 15:30:57 -0800

    update action

--- frontend/src/actions/addUser.js
+++ frontend/src/actions/addUser.js
@@ -1,18 +1,18 @@
-export const addUser = (userData) => {
+export const addUser = userData => {
   return async dispatch => {
-    const response = await fetch('http://localhost:3000/api/v1/users', {
+    const response = await fetch('http://localhost:3001/api/v1/users', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       },
-      body: JSON.stringify(userData )
-    })
+      body: JSON.stringify({user: userData})
+    });
     const data = await response.json();
     if (data.error) {
       // add error handling
     } else {
-      localStorage.setItem('token', data.token)
+      localStorage.setItem('token', data.jwt)
       dispatch({ type: 'LOGIN_USER', payload: data.user })
     }
   }
