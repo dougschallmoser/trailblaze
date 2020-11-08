6e16442307f5c8aa72c860bc08ee53a10295a197
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-08 15:33:24 -0800

    remove router from App

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-import reducers from './reducers';
+import rootReducer from './reducers';
 import App from './App';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+let store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
       <App />
-    </Router>
   </Provider>, 
   document.querySelector('#main')
 );
