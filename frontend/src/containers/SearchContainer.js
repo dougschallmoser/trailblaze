6e5c657d69c31c14811fa1ed2c9b3aa10f54467b
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-07 18:55:20 -0800

    build component by importing other components

--- frontend/src/containers/SearchContainer.js
+++ frontend/src/containers/SearchContainer.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import LocationSearch from '../components/LocationSearch';
+import DateSearch from '../components/DateSearch';
 
 const SearchContainer = () => {
   return (
-    <div className="container">Search</div>
+    <div className="container">
+      <div className="searchbar">
+        <LocationSearch />
+        <DateSearch />
+      </div>
+    </div>
   )
 }
 
