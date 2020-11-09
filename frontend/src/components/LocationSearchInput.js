359416b5b9bbf01951cac7cabf4a52ac215083de
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-09 13:34:53 -0800

    refactor class component into function component

--- frontend/src/components/LocationSearchInput.js
+++ frontend/src/components/LocationSearchInput.js
@@ -1,74 +1,69 @@
 import React from 'react';
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng,
-} from 'react-places-autocomplete';
+import { useState } from 'react';
+import PlacesAutocomplete, { geocodeByAddress,getLatLng } from 'react-places-autocomplete';
+
+const LocationSearchInput = () => {
+  
+  const [address, setAddress] = useState('')
  
-class LocationSearchInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { address: '' };
-  }
- 
-  handleChange = address => {
-    this.setState({ address });
+  const handleChange = input => {
+    setAddress(input)
   };
  
-  handleSelect = address => {
-    geocodeByAddress(address)
+  const handleSelect = selection => {
+    geocodeByAddress(selection)
       .then(results => getLatLng(results[0]))
       .then(latLng => console.log('Success', latLng))
       .catch(error => console.error('Error', error));
   };
 
-  searchOptions = {
+  const searchOptions = {
     types: ['(cities)'],
     componentRestrictions: {country: "us"}
   }
- 
-  render() {
-    return (
-      <PlacesAutocomplete
-        debounce={500}
-        value={this.state.address}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-        searchOptions={this.searchOptions}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div>
-            <input
-              {...getInputProps({
-                placeholder: 'Where would you like to find Trailblazers',
-                className: 'location-search-input',
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map(suggestion => {
-                const className = suggestion.active
-                  ? 'suggestion-item--active'
-                  : 'suggestion-item';
-                const style = suggestion.active
-                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style,
-                    })}
-                  >
-                    <span>{suggestion.description}</span>
-                  </div>
-                );
-              })}
-            </div>
+
+  return (
+    <PlacesAutocomplete
+      debounce={500}
+      value={address}
+      onChange={handleChange}
+      onSelect={handleSelect}
+      searchOptions={searchOptions}
+    >
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <div>
+          <input
+            {...getInputProps({
+              placeholder: 'Where would you like to find Trailblazers',
+              className: 'location-search-input',
+            })}
+          />
+          <div className="autocomplete-dropdown-container">
+            {loading && <div>Loading...</div>}
+            {suggestions.map(suggestion => {
+              const className = suggestion.active
+                ? 'suggestion-item--active'
+                : 'suggestion-item';
+              const style = suggestion.active
+                ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                : { backgroundColor: '#ffffff', cursor: 'pointer' };
+              return (
+                <div
+                  {...getSuggestionItemProps(suggestion, {
+                    className,
+                    style,
+                  })}
+                  key={suggestion.placeId}
+                >
+                  <span>{suggestion.description}</span>
+                </div>
+              );
+            })}
           </div>
-        )}
-      </PlacesAutocomplete>
-    );
-  }
+        </div>
+      )}
+    </PlacesAutocomplete>
+  )
 }
 
 export default LocationSearchInput;
