de686429f07c1f55b600f4c39b0d2d12ed7b35ac
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-09 14:55:39 -0800

    add initial state variable

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,9 +1,16 @@
-const userReducer = (state = {currentUser: {}}, action) => {
+const initialState = {
+  currentUser: {},
+  searchResults: []
+};
+
+const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'LOGIN_USER':
       return {...state, currentUser: action.payload}
     case 'LOGOUT_USER':
       return {...state, currentUser: {}}
+    case 'SEARCH_RESULTS':
+      return {...state, searchResults: action.payload }
     default:
       return state;
   }
