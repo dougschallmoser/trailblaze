8e35e215197f991b755afd91a2b39e528085a866
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-09 14:56:28 -0800

    add component to house results

---
+++ frontend/src/containers/ResultsContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RequestList from '../components/requests/RequestList';
+
+const ResultsContainer = () => {
+  
+  return (
+    <div>
+      <RequestList />
+    </div>
+  )
+}
+
+export default ResultsContainer;
