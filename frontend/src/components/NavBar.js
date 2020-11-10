e31e6ec6bb3a2cd8c18a05ed5f41c3e03cfd469a
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-10 11:47:17 -0800

    remove link to lougout and instead add button with onClick function

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logoutUser } from '../actions';
 
 const NavBar = () => {
 
   const user = useSelector(state => state.users);
   const theState = useSelector(state => state)
+  const dispatch = useDispatch();
   console.log('current state is:', theState)
+
+  const handleLogout = () => {
+    dispatch(logoutUser())
+  }
 
   return (
     <div className="navbar">
       <Link to="/" className="nav-logo">Trailblaze</Link>
       <Link to="/signup">Signup</Link>
       <Link to="/login">Login</Link>
-      <Link to="/logout">Logout</Link>
+      <button onClick={handleLogout}>Logout</button>
       <Link to="/map">Map</Link>
       <span id="current-user">Current User: {user.currentUser && user.currentUser.username}</span>
     </div>
