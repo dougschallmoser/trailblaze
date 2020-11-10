a69ed4f9381fc720a9b1be6b1ad0a3f3d8814956
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-10 10:00:32 -0800

    add commented out code to update search results with global state

--- frontend/src/containers/SearchContainer.js
+++ frontend/src/containers/SearchContainer.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import LocationSearchInput from '../components/LocationSearchInput';
 import LocationRadiusInput from '../components/LocationRadiusInput';
 
 const SearchContainer = () => {
 
+  // const [query, setQuery] = useState({coordinates: {}, radius: ''})
+  // const dispatch = useDispatch();
+
+  // const updateRadius = (rad) => {
+  //   setQuery({radius: rad})
+  // }
+
+  // const updateCoordinates = (coords) => {
+  //   setQuery({ coordinates: coords })
+  // }
+
   return (
     <div className="container">
       <div className="">
+        {/* <LocationRadiusInput updateRadius={updateRadius} />
+        <LocationSearchInput updateCoordinates={updateCoordinates} /> */}
         <LocationRadiusInput />
         <LocationSearchInput />
       </div>
