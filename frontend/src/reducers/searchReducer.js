0082a4a0533ed41651af603ea2227c3dc762a47d
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-10 13:23:58 -0800

    add cases for updating radius and coordinates

--- frontend/src/reducers/searchReducer.js
+++ frontend/src/reducers/searchReducer.js
@@ -6,7 +6,11 @@ const initialState = {
 const searchReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SEARCH_RESULTS':
-      return {...state, results: action.payload }
+      return { ...state, results: action.payload }
+    case 'UPDATE_RADIUS':
+      return { ...state, query: {...state.query, radius: action.payload} }
+      case 'UPDATE_COORDINATES':
+        return { ...state, query: {...state.query, ...action.payload} }
     default:
       return state;
   }
