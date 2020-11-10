9ef9146c7acdd8b077a6109e97b54ae5a53add67
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-09 18:22:41 -0800

    fix find_by and authenticate params

--- backend/app/controllers/api/v1/auth_controller.rb
+++ backend/app/controllers/api/v1/auth_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    user = User.find_by(username: login_params[:username])
-    if user && user.authenticate(login_params[:password])
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
       token = encode_token({ user_id: user.id })
       render json: { user: UserSerializer.new(user), jwt: token }, status: :accepted
     else 
