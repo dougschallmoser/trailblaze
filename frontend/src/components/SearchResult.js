cd853ef4a2019ab94357a0d442753fb5b471041e
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-10 18:10:07 -0800

    add function to calculate user age

--- frontend/src/components/SearchResult.js
+++ frontend/src/components/SearchResult.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import faker from 'faker';
 
 const SearchResult = ({ user }) => {
+
+  const userAge = () => {
+    const birthDate = new Date(user.dob)
+    const difference = Date.now() - birthDate.getTime()
+    const age = new Date(difference)
+    return Math.abs(age.getUTCFullYear() - 1970);
+  }
+
   return (
     <div>
-      <img src={user.avatar} alt="avatar" /><br/>
-      {user.username}<br/>
+      <img src={faker.image.avatar()} alt="avatar" /><br/>
+      {user.username}, {userAge()}<br/>
       {user.bio}
     </div>
   )
