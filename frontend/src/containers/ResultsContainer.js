4c08833e36cb0d706b08244b481e4602125eb647
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-11 11:21:46 -0800

    add ternary operator to render GoogleMap component when there are results

--- frontend/src/containers/ResultsContainer.js
+++ frontend/src/containers/ResultsContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import GoogleMap from '../components/GoogleMap';
 import SearchResult from '../components/SearchResult';
 
 const ResultsContainer = () => {
@@ -7,12 +8,19 @@ const ResultsContainer = () => {
   const results = useSelector(state => state.search.results)
   
   return (
-    <div>
+    <>
       <div className="result-length">
         {results.length} trailblazers were found
       </div>
-      {results.map(user => <SearchResult user={user} key={user.id} />)}
-    </div>
+      <div className="results">
+        <div className="items">
+          {results.map(user => <SearchResult user={user} key={user.id} />)}
+        </div>
+        <div className="google-map">
+          {results.length > 0 ? <GoogleMap /> : null }
+        </div>
+      </div>
+    </>
   )
 }
 
