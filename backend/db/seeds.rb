f24ade914d73064add443c7af9d0d8945b3bd5da
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-10 17:06:56 -0800

    update seed data, remove unnecessary gem

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,26 +5,79 @@ require 'faker'
 User.create(
   username: "doug",
   password: "monkey",
-  bio: Faker::Lorem.sentence(word_count: 20),
+  bio: Faker::Hipster.sentence(word_count: 25),
   avatar: Faker::Avatar.image(size: "100x100"),
   lat: 48.752230399999995,
   lng: -122.49006080000001
-  )
+)
 
   User.create(
   username: "erin",
   password: "monkey",
-  bio: Faker::Lorem.sentence(word_count: 20),
+  bio: Faker::Hipster.sentence(word_count: 25),
   avatar: Faker::Avatar.image(size: "100x100"),
   lat: 47.261058,
   lng: -122.167780
-  )
+)
 
-  User.create(
-  username: "mom",
-  password: "monkey",
-  bio: Faker::Lorem.sentence(word_count: 20),
-  avatar: Faker::Avatar.image(size: "100x100"),
-  lat: 48.043431,
-  lng: -120.847285
-  )
+# BELLINGHAM
+
+userss_list = []
+
+10.times do
+  username = Faker::Name.first_name
+  password = Faker::Internet.password(min_length: 8)
+  bio = Faker::Hipster.sentence(word_count: 25)
+  avatar = Faker::Avatar.image(size: "100x100")
+  lat = 48.762696
+  lng = -122.436228
+
+  userss_list << [username, password, bio, avatar, lat, lng]
+
+end
+
+userss_list.each do |username, password, bio, avatar, lat, lng|
+  User.create(username: username, password: password, bio: bio, avatar: avatar, lat: lat, lng: lng)
+end
+
+
+# SEATTLE
+
+user_list = []
+
+10.times do
+  username = Faker::Name.first_name
+  password = Faker::Internet.password(min_length: 8)
+  bio = Faker::Hipster.sentence(word_count: 25)
+  avatar = Faker::Avatar.image(size: "100x100")
+  lat = 47.607957
+  lng = -122.322040
+
+  user_list << [username, password, bio, avatar, lat, lng]
+
+end
+
+user_list.each do |username, password, bio, avatar, lat, lng|
+  User.create(username: username, password: password, bio: bio, avatar: avatar, lat: lat, lng: lng)
+end
+
+
+# PORTLAND
+
+users_list = []
+
+10.times do
+  username = Faker::Name.first_name
+  password = Faker::Internet.password(min_length: 8)
+  bio = Faker::Hipster.sentence(word_count: 25)
+  avatar = Faker::Avatar.image(size: "100x100")
+  lat = 45.460522
+  lng = -122.611955
+
+  users_list << [username, password, bio, avatar, lat, lng]
+
+end
+
+users_list.each do |username, password, bio, avatar, lat, lng|
+  User.create(username: username, password: password, bio: bio, avatar: avatar, lat: lat, lng: lng)
+end
