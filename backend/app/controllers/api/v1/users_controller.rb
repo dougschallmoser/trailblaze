cac1f67dbe41573d05f1ab847303accbbcc2c779
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-10 18:08:39 -0800

    add dob to params

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -26,7 +26,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :bio, :avatar)
+    params.require(:user).permit(:username, :password, :bio, :dob, :avatar)
   end
 
 end
