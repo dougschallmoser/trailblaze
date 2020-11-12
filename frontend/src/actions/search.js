1d3009fecf56db8cc2e21a746b5911bb586354f6
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-11 20:15:49 -0800

    add action creator and reducer for fetching trails

--- frontend/src/actions/search.js
+++ frontend/src/actions/search.js
@@ -31,3 +31,18 @@ export const getUsers = queryData => {
     }
   }
 }
+
+export const getTrails = queryData => {
+  return async dispatch => {
+    const response = await fetch(`https://www.hikingproject.com/data/get-trails?lat=${queryData.lat}&lon=${queryData.lng}&maxDistance=${queryData.radius}&maxResults=50&key=200850712-41bb2ec1278a205fdc5c9050b10c3ad2`)
+    const data = await response.json();
+    if (data.error) {
+      // add error handling
+      console.log('error')
+    } else {
+      console.log('success')
+      console.log(data)
+      dispatch({ type: 'GET_TRAILS', payload: data.trails })
+    }
+  }
+}
--- frontend/src/reducers/searchReducer.js
+++ frontend/src/reducers/searchReducer.js
@@ -1,6 +1,7 @@
 const initialState = {
   results: [],
-  query: {}
+  query: {},
+  trails: []
 };
 
 const searchReducer = (state = initialState, action) => {
@@ -8,9 +9,11 @@ const searchReducer = (state = initialState, action) => {
     case 'SEARCH_RESULTS':
       return { ...state, results: action.payload }
     case 'UPDATE_RADIUS':
-      return { ...state, query: {...state.query, radius: action.payload} }
-      case 'UPDATE_LOCATION':
-        return { ...state, query: {...state.query, ...action.payload} }
+      return { ...state, query: {...state.query, radius: action.payload }}
+    case 'UPDATE_LOCATION':
+      return { ...state, query: { ...state.query, ...action.payload }}
+    case 'GET_TRAILS':
+      return { ...state, trails: action.payload }
     default:
       return state;
   }
