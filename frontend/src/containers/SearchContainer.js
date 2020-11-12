956b817733cda8b4458ff434a5c4083bb1a3a3cc
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-12 13:02:53 -0800

    render SearchAge component

--- frontend/src/containers/SearchContainer.js
+++ frontend/src/containers/SearchContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchRadius from '../components/SearchRadius';
 import SearchBar from '../components/SearchBar';
+import SearchAge from '../components/SearchAge';
 
 const SearchContainer = () => {
 
@@ -8,6 +9,7 @@ const SearchContainer = () => {
     <div className="search">
       <SearchBar />
       <SearchRadius />
+      <SearchAge />
     </div>
   )
 }
