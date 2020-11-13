9a683683dacb93e1ec56e3dd34e7f0fb1dfbeb99
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-12 17:51:18 -0800

    add an instance method to convert dob to age

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password 
   # validates :username, uniqueness: { case_sensitive: false }
   acts_as_mappable
+
+  def age
+    ((Time.zone.now - self.dob.to_time) / 1.year.seconds).floor
+  end
 end
