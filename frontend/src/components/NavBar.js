2e98bd21e9ca2cd9d9df2eba636f03779d2491d0
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-13 20:15:29 -0800

    refactor nav bar to include nav menu

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { logoutUser } from '../actions';
-import UserSignup from './UserSignup';
+import NavMenu from './NavMenu';
 
 const NavBar = () => {
 
-  const currentUser = useSelector(state => state.currentUser);
   const theState = useSelector(state => state)
-  const dispatch = useDispatch();
   console.log('current state is:', theState)
 
-  const handleLogout = () => {
-    dispatch(logoutUser())
-  }
-
   return (
-    <div className="navbar">
-      <Link to="/" className="nav-logo">Trailblaze</Link>
-      <UserSignup />
-      <Link to="/login">Login</Link>
-      <button onClick={handleLogout}>Logout</button>
+    <>
+      <NavMenu />
+      <div className="navbar">
+      <Link to="/" className="nav-logo"><span>Trail</span>blaze</Link>
       <Link to="/map">Map</Link>
-      <span id="current-user">Current User: {currentUser && currentUser.email}</span>
     </div>
+    </>
   )
 }
 
