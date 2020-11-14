d73c2c3f53af55465fb8faecf54eb093a044e70d
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-13 16:40:35 -0800

    reconfigure index reducer to reflect change in user reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -5,7 +5,7 @@ import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   request: requestReducer,
-  users: userReducer,
+  currentUser: userReducer,
   search: searchReducer
 });
 
