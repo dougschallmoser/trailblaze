d41f76b14405e01e282c245195c3f2c257e9f316
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-14 14:06:44 -0800

    updates associations and validations

--- backend/app/models/conversation.rb
+++ backend/app/models/conversation.rb
@@ -1,3 +1,6 @@
 class Conversation < ApplicationRecord
-  has_many :messages
+  has_many :messages, -> { order(created_at: :asc) }, dependent: :destroy
+  belongs_to :author, class_name: 'User'
+  belongs_to :receiver, class_name: 'User', optional: true
+  validates :author, uniqueness: {scope: :receiver}
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password 
+  has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
+  has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
+  has_many :messages, dependent: :destroy
   validates :email, uniqueness: true
   validates :name, :email, :bio, :gender, :dob, :avatar, :lat, :lng, presence: true
   acts_as_mappable
