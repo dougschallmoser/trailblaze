0a590404b138f2ac45220b9451ebdb2228b8e5cc
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-16 13:01:26 -0800

    add created_at attribute to serializer

--- backend/app/serializers/conversation_serializer.rb
+++ backend/app/serializers/conversation_serializer.rb
@@ -1,4 +1,4 @@
 class ConversationSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :receiver, :accepted
+  attributes :id, :title, :author, :receiver, :accepted, :created_at
   has_many :messages
 end
