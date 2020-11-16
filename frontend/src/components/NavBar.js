d6130d6a3803286932e3b8d74220c009ac58e1ae
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-16 15:48:22 -0800

    remove useSelector and console.log

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import NavMenu from './NavMenu';
 
 const NavBar = () => {
 
-  const theState = useSelector(state => state)
-  console.log('current state is:', theState)
-
   return (
     <>
       <NavMenu />
       <div className="navbar">
-      <Link to="/" className="nav-logo"><span>Trail</span>blaze</Link>
-    </div>
+        <Link to="/" className="nav-logo"><span>Trail</span>blaze</Link>
+      </div>
     </>
   )
 }
