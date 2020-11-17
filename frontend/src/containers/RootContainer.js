a059f63d8c0bd42d8d49e9fbd453a8421869fff2
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-16 20:05:47 -0800

    add container component for rendering root page

---
+++ frontend/src/containers/RootContainer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+const RootContainer = () => {
+
+  return (
+    <div className="root-page">
+      <div className="centered">
+        <div className="splash-message">Trailblaze together</div>
+        <div className="splash-submessage">Find friendships for your next adventure</div>
+        <SearchBar
+          splash="splash"
+          splashContainer="search-splash-container"
+          splashIcon="search-icon-splash"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default RootContainer;
