2bfb54bb9315301f824658fd72e292f48100d749
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-17 08:20:32 -0800

    remove unused reducer and import of reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,8 @@
 import userReducer from './userReducer';
-import requestReducer from './requestReducer';
 import searchReducer from './searchReducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  request: requestReducer,
   currentUser: userReducer,
   search: searchReducer
 });
