b591b012c6dd70d70592ffc6db79cf63bf98b1f7
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-18 11:58:17 -0800

    add errors object to rootReducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,12 @@
 import userReducer from './userReducer';
 import searchReducer from './searchReducer';
+import errorReducer from './errorReducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   currentUser: userReducer,
-  search: searchReducer
+  search: searchReducer,
+  errors: errorReducer
 });
 
 export default rootReducer
