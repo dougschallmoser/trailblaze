8cf9a72c1c14fa8f64fd3c701bc09b73d60d6a05
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-18 10:47:48 -0800

    remove HEADERS constant

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,6 +1,2 @@
 export const API_ROOT = 'http://localhost:3001/api/v1';
 export const API_WS_ROOT = 'ws://localhost:3001/api/v1/cable';
-export const HEADERS = {
-  'Content-Type': 'application/json',
-  Accept: 'application/json',
-};
