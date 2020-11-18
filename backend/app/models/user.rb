477d2bc89bb749fce87cd656e3fca45eed72f00c
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-18 13:46:10 -0800

    add custom message for lat lng error

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
   has_many :messages, dependent: :destroy
   validates :email, uniqueness: true
-  validates :name, :email, :bio, :gender, :dob, :avatar, :lat, :lng, presence: true
+  validates :name, :email, :bio, :gender, :dob, :avatar, presence: true
+  validates_presence_of :lat, :message => "- You must allow Trailblaze access to your location"
+  validates_presence_of :lng, :message => "- You must allow Trailblaze access to your location"
   before_save { name.capitalize() }
   acts_as_mappable
 
