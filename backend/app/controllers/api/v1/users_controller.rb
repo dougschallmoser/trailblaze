ef4422013d88fc8b419d41fa006e22079573ad49
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-19 15:13:57 -0800

    remove status when else statement is executed

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -37,7 +37,7 @@ class Api::V1::UsersController < ApplicationController
       token = encode_token(user_id: user.id)
       render json: { user: UserSerializer.new(user), jwt: token }, status: :created
     else
-      render json: { error: user.errors.full_messages }, status: :not_acceptable
+      render json: { error: user.errors.full_messages }
     end
   end
 
