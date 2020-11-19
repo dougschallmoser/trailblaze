7f0db66f7ce6d3abedaafcdcb44d6ac2b2d1276c
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-18 17:07:18 -0800

    reset the database

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_183743) do
+ActiveRecord::Schema.define(version: 2020_11_18_231858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,24 @@ ActiveRecord::Schema.define(version: 2020_11_14_183743) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id", "receiver_id"], name: "index_conversations_on_author_id_and_receiver_id", unique: true
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "length"
+    t.string "ascent"
+    t.integer "low"
+    t.integer "high"
+    t.string "url"
+    t.string "summary"
+    t.string "imgmed"
+    t.string "imgsmall"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "messages", force: :cascade do |t|
