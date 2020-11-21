914fc37b3a687a1b917e7dae1afec4205d9e6276
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-21 13:16:50 -0800

    remove Error component, remove error reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,12 +1,10 @@
 import userReducer from './userReducer';
 import searchReducer from './searchReducer';
-import errorReducer from './errorReducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   currentUser: userReducer,
-  search: searchReducer,
-  errors: errorReducer
+  search: searchReducer
 });
 
 export default rootReducer
