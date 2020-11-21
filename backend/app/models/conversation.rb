9fba55f9e15a43e99756c4f114441f5baa693919
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-21 11:02:47 -0800

    build scope method for pulling conversations from particular user

--- backend/app/models/conversation.rb
+++ backend/app/models/conversation.rb
@@ -3,4 +3,5 @@ class Conversation < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :receiver, class_name: 'User', optional: true
   validates :author, uniqueness: {scope: :receiver}
+  scope :by_user, -> (id) { where(author_id: id).or(Conversation.where(receiver_id: id)) }
 end
