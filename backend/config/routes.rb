1dfef3e372d98f43cf057b704b64e61fc7aa9a4a
Doug Schallmoser <dougschallmoser@gmail.com>
2020-11-29 21:43:45 -0800

    organize routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
+      mount ActionCable.server => '/cable'
       post '/search', to: 'users#search'
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
-      mount ActionCable.server => '/cable'
       resources :users, only: [:create] do 
         resources :conversations, shallow: true
         resources :favorites, shallow: true
