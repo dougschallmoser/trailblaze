d8703986269b813cae382467dcee87deb993cf4c
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-09 17:51:00 -0800

    change constants to heroku url

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,2 +1,2 @@
-export const API_ROOT = 'http://localhost:3001/api/v1';
-export const API_WS_ROOT = 'ws://localhost:3001/api/v1/cable';
+export const API_ROOT = 'https://trailblaze-app.herokuapp.com/api/v1';
+export const API_WS_ROOT = 'ws://trailblaze-app.herokuapp.com/api/v1/cable';
