ce74fb7e2cb6eaa5a82050cce271ed6ffc2b9f50
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-13 13:22:05 -0800

    add commented lines for constants, update mobile filter button styling

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,2 +1,6 @@
 export const API_ROOT = 'https://trailblaze-app.herokuapp.com/api/v1';
 export const API_WS_ROOT = 'wss://trailblaze-app.herokuapp.com/api/v1/cable';
+
+// Replace code above with this for local environment:
+// export const API_ROOT = 'http://localhost:3001/api/v1';
+// export const API_WS_ROOT = 'ws://localhost:3001/api/v1/cable';
