269afee2112d21fc40081fe7dee22ab9821b7273
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-21 11:08:02 -0800

    add loading key value pair

--- frontend/src/reducers/searchReducer.js
+++ frontend/src/reducers/searchReducer.js
@@ -1,4 +1,5 @@
 const initialState = {
+  loading: true,
   results: [],
   query: {
     city: '',
@@ -15,7 +16,7 @@ const initialState = {
 const searchReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SEARCH_RESULTS':
-      return { ...state, results: action.payload }
+      return { ...state, results: action.payload, loading: false }
     case 'UPDATE_QUERY':
       return { ...state, query: { ...state.query, ...action.payload }}
     case 'GET_TRAILS':
